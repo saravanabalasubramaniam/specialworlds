46939a5ece442020fa395012199e9b6dcffb4a4c
saravana <saravanasaru14313@gmail.com>
2019-02-06 03:24:10 -0800

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Specialcount
+{
+public static void main(String args[])
+	{
+	Scanner input=new Scanner(System.in);
+	String a=input.nextLine();
+	int count=0;
+	for(int i=0;i<a.length();i++)
+	{
+	    if((a.charAt(i)>='0'||a.charAt(i)>='a')&&(a.charAt(i)<='9'||a.charAt(i)<='z'))
+	    {
+		
+	     }
+		else
+		{
+		count++;
+	}}
+	System.out.println(count);
+}}
